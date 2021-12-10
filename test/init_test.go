4a7fbbd5f612af1069a984d9817a1a5203484064
Iris Chen <10179943+iyabchen@users.noreply.github.com>
2021-12-10 13:16:31 -0500

    Bump up test version to 4.2.0 and adjust test data files

--- test/init_test.go
+++ test/init_test.go
@@ -21,7 +21,7 @@ const (
 	defaultOrganization    = "12345"
 	defaultFolder          = "67890"
 	defaultProject         = "foobar"
-	defaultProviderVersion = "3.84.0"
+	defaultProviderVersion = "4.2.0"
 )
 
 var (
